9c2532023d3af43da35517a9a673d13c2d7af547
Ederson Torresini <boidacarapreta@gmail.com>
2023-10-30 21:25:33 +0000

    Testar JWT no servidor

--- routes/postExtrato.js
+++ routes/postExtrato.js
@@ -1,21 +1,20 @@
 const express = require('express')
 const router = express.Router()
-const Joi = require('joi')
-const bodyParser = require('body-parser')
-router.use(bodyParser.json())
+const { OAuth2Client } = require('google-auth-library')
+const client = new OAuth2Client()
 const db = require('../db.js')
 
-const extratoEsquema = Joi.object({
-  id: Joi.number().integer().min(0).required(),
-  senha: Joi.number().integer().min(0).required()
-})
+async function verify () {
+  const ticket = await client.verifyIdToken({
+    idToken: 'token',
+    audience: 'pnkivbritoue59pbodppmifihnqe2tpt.apps.googleusercontent.com'
+  })
+  const payload = ticket.getPayload()
+  const userid = payload.sub
+}
 
 router.post('/extrato', async (req, res) => {
-  const validationResult = extratoEsquema.validate(req.body)
-  if (validationResult.error) {
-    res.status(400).send(validationResult.error.details[0].message)
-    return
-  }
+  verify().catch(console.error)
 
   try {
     const auth = await db.query('SELECT id FROM jogadores WHERE id = $1 AND senha = $2', [req.body.id, req.body.senha])
