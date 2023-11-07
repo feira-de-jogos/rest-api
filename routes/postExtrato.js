6a1e0b1df57b1daca47c665f7fb8fe82dfc5af73
leojungneves <leojn132013@gmail.com>
2023-11-07 09:58:16 -0300

    teste funcional

--- routes/postExtrato.js
+++ routes/postExtrato.js
@@ -21,53 +21,177 @@ router.post('/extrato', async (req, res) => {
     }
     // id = id.rows[0].id
 
-    let receitas = await db.query('SELECT jogos.nome AS jogo, to_char(receitas.data, \'DD/MM/YYYY HH24:MI:SS\') AS data, receitas.valor FROM receitas INNER JOIN jogos ON jogos.id = receitas.jogo_id WHERE receitas.jogador_id = $1', [id.rows[0].id])
-    receitas = { receitas: receitas.rows }
+    // let receitas = await db.query('SELECT jogos.nome AS jogo, to_char(receitas.data, \'DD/MM/YYYY HH24:MI:SS\') AS data, receitas.valor FROM receitas INNER JOIN jogos ON jogos.id = receitas.jogo_id WHERE receitas.jogador_id = $1', [id.rows[0].id])
+    // receitas = { receitas: receitas.rows }
 
-    let despesas = await db.query('SELECT produtos.descricao AS produto, to_char(despesas.data, \'DD/MM/YYYY HH24:MI:SS\') AS data, despesas.valor FROM despesas INNER JOIN produtos ON produtos.id = despesas.produto_id WHERE despesas.jogador_id = $1', [id.rows[0].id])
-    despesas = { despesas: despesas.rows }
+    // let despesas = await db.query('SELECT produtos.descricao AS produto, to_char(despesas.data, \'DD/MM/YYYY HH24:MI:SS\') AS data, despesas.valor FROM despesas INNER JOIN produtos ON produtos.id = despesas.produto_id WHERE despesas.jogador_id = $1', [id.rows[0].id])
+    // despesas = { despesas: despesas.rows }
 
-    const formatReceitas = receitas.receitas.map((receita) => {
-      return `
-        <div>
-          <p>Jogo: ${receita.jogo}</p>
-          <p>Data: ${receita.data}</p>
-          <p>Valor: ${receita.valor}</p>
-        </div>
-      `
-    }).join('')
+    const extratoMontado = await db.query(`
+    SELECT
+    'Receita' AS tipo,
+    jogo.nome AS transacao,
+    receitas.valor AS valor,
+    receitas.data AS data
+  FROM
+    receitas
+  INNER JOIN
+    jogo ON jogo.id = receitas.jogo_id
+  WHERE
+    receitas.jogador_id = (SELECT id FROM jogadores WHERE id = $1)
 
-    const formatDespesas = despesas.despesas.map((despesa) => {
-      return `
-        <div>
-          <p>Produto: ${despesa.produto}</p>
-          <p>Data: ${despesa.data}</p>
-          <p>Valor: ${despesa.valor}</p>
-        </div>
-      `
-    }).join('')
+  UNION ALL
 
-    // Crie a página HTML com os dados formatados
-    const htmlpage = `
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Extrato</title>
-      </head>
-      <body>
-        <h1>Extrato</h1>
-        <h2>Receitas</h2>
-        ${formatReceitas}
-        <h2>Despesas</h2>
-        ${formatDespesas}
-      </body>
-      </html>
+  SELECT
+    'Despesa' AS tipo,
+    produtos.descricao AS transacao,
+    despesas.valor AS valor,
+    despesas.data AS data
+  FROM
+    despesas
+  INNER JOIN
+    produto ON produtos.id = despesas.produto_id
+  WHERE
+    despesas.jogador_id = (SELECT id FROM jogadores WHERE id = $1')
+  
+  ORDER BY
+    data DESC;`, [id.rows[0].id])
+
+    let pagehtml = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <style>
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #f5f5f5;
+      }
+      .container {
+        align-items: center;
+        background-color: #ffffff;
+        margin: 20px;
+        padding: 20px;
+        border-radius: 10px;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+        max-width: 800px;
+        width: 100%;
+        margin-top: 50px;
+        margin-left: auto;
+        margin-right: auto;
+      }
+      .receita-container, .despesa-container {
+        background-color: #ffffff;
+        margin-bottom: 20px;
+        padding: 20px;
+        border-radius: 10px;
+        border: 1px solid #eaeaea;
+      }
+      .receita-container p, .despesa-container p {
+        margin: 5px 0;
+        color: #333;
+      }
+      h1 {
+        text-align: center;
+        padding-bottom: 20px;
+        color: #333;
+      }
+      .receita-container {
+        border-left: 5px solid #00a400;
+      }
+      .despesa-container {
+        border-left: 5px solid #e84855;
+      }
+      .nav-bar {
+        background-color: #fff;
+        border-bottom: 1px solid #ddd;
+        padding: 10px;
+        box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        flex-wrap: wrap;
+      }
+      .nav-links {
+        list-style: none;
+        margin: 10px 0;
+        display: flex;
+        flex-wrap: wrap;
+      }
+      .nav-links li {
+        margin: 0 20px;
+      }
+      .nav-links a {
+        text-decoration: none;
+        color: #333;
+        font-size: 18px;
+        font-weight: bold;
+      }
+      .nav-links a:hover {
+        color: #3483fa;
+      }
+      .user-actions {
+        display: flex;
+        align-items: center;
+        margin-top: 10px;
+      }
+      .user-actions a {
+        text-decoration: none;
+        color: #333;
+        font-size: 18px;
+        font-weight: bold;
+        margin-left: 20px;
+      }
+      .user-actions a:hover {
+        color: #3483fa;
+      }
+      .saldo {
+        text-decoration: none;
+        color: #333;
+        font-size: 18px;
+        font-weight: bold;
+      }
+      </style>
+    </head>
+    <body>
+    <div class="nav-bar">
+      <ul class="nav-links">
+          <li><a href="/receitas">Receitas</a></li>
+          <li><a href="/despesas">Despesas</a></li>
+          <li><a href="/extrato">Extrato</a></li>
+          <li><a href="/produtos">Produtos</a></li>
+      </ul>
+      <div class="user-actions">
+         <p class="saldo">Saldo: TJ$ Temp indisponivel</p>
+      </div>
+    </div>
+    <div class="container">
+      <h1>Extrato</h1>
+  `
+
+    extratoMontado.rows.forEach(row => {
+      const tipoLower = row.tipo.toLowerCase()
+      const tipoExibicao = tipoLower === 'receita' ? 'Valor ganho' : 'Valor gasto'
+      const dataFormatada = row.data
+
+      pagehtml += `
+      <div class="${tipoLower}-container">
+        <p><strong>Tipo:</strong> ${row.tipo}</p>
+        <p><strong>Transação:</strong> ${row.transacao}</p>
+        <p><strong>${tipoExibicao}:</strong> ${row.valor} Tijolinhos</p>
+        <p><strong>Data:</strong> ${dataFormatada} </p>
+      </div>
     `
+    })
+
+    pagehtml += '</div></body></html>'
 
     // Envie a página HTML formatada como resposta
-    res.send(htmlpage)
+    res.send(pagehtml)
   } catch (err) {
     res.sendStatus(500)
   }
