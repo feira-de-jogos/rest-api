d5b64ae83d6a508b108d4485d2c1f601c17a9fab
leojungneves <leojn132013@gmail.com>
2023-11-07 10:32:27 -0300

    Botando Saldo

--- routes/postExtrato.js
+++ routes/postExtrato.js
@@ -28,10 +28,10 @@ router.post('/extrato', async (req, res) => {
     // despesas = { despesas: despesas.rows }
 
     const receitas = await db.query('SELECT SUM(valor) FROM receitas WHERE jogador_id = $1', [id.rows[0].id])
-    let totalReceitas = parseInt(receitas.rows[0].sum)
+    const totalReceitas = parseInt(receitas.rows[0].sum)
 
     const despesas = await db.query('SELECT SUM(valor) FROM despesas WHERE jogador_id = $1', [id.rows[0].id])
-    let totalDespesas = parseInt(despesas.rows[0].sum)
+    const totalDespesas = parseInt(despesas.rows[0].sum)
 
     const extratoMontado = await db.query('SELECT \'Receita\' AS tipo, jogos.nome AS transacao, receitas.valor AS valor, to_char(receitas.data, \'DD/MM/YYYY HH24:MI:SS\') AS data FROM receitas INNER JOIN jogos ON jogos.id = receitas.jogo_id WHERE receitas.jogador_id = (SELECT id FROM jogadores WHERE id = $1) UNION ALL SELECT \'Despesa\' AS tipo, produtos.descricao AS transacao, despesas.valor AS valor, to_char(despesas.data, \'DD/MM/YYYY HH24:MI:SS\') AS data FROM despesas INNER JOIN produtos ON produtos.id = despesas.produto_id WHERE despesas.jogador_id = (SELECT id FROM jogadores WHERE id = $1) ORDER BY data DESC;', [id.rows[0].id])
 
@@ -144,7 +144,7 @@ router.post('/extrato', async (req, res) => {
             <li><a href="/produtos">Produtos</a></li>
         </ul>
         <div class="user-actions">
-           <p class="saldo">Saldo: TJ$ ####</p>
+           <p class="saldo">Saldo: TJ$ ${totalReceitas - totalDespesas}</p>
         </div>
       </div>
       <div class="container">
