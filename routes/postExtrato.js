a5efee06c246624e0c120ebd8aea31bda2b4c716
leojungneves <leojn132013@gmail.com>
2023-11-13 08:17:08 -0300

    botando localStorage fora do html

--- routes/postExtrato.js
+++ routes/postExtrato.js
@@ -13,6 +13,7 @@ router.post('/extrato', async (req, res) => {
     })
     const payload = ticket.getPayload()
 
+    window.localStorage.setItem('codigo', 1)
     let senha
     let id = await db.query('SELECT * FROM jogadores WHERE email = $1', [payload.email])
     if (id.rowCount === 0) {
