8da1d2a4bf132199bfb6f320d4f6068a972a2c8b
leojungneves <leojn132013@gmail.com>
2023-11-27 22:51:07 -0300

    alterando titulo da pagina adm

--- routes/postControleAdm.js
+++ routes/postControleAdm.js
@@ -63,7 +63,7 @@ router.get('/adm', async (req, res) => {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Minha Conta</title>
+  <title>Página Admin</title>
   <link rel="icon" href="/img/Banco-Imagem.ico" type="image/ico">
   <style>
     /* Reset de estilos */
