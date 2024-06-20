d9a2c5339900c7907ab9014aea97ebac54a00af7
Léo Jung Neves <leo.jn13@aluno.ifsc.edu.br>
2024-06-20 04:29:56 +0000

    Organizar código

--- servidor.js
+++ servidor.js
@@ -3,18 +3,16 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
+const postLogin = require('./routes/postLogin')
 
 app.use(cors({
   origin: [
+    /postman\.co$/,
     /github\.dev$/,
     /feira-de-jogos\.dev\.br$/
   ],
   methods: 'POST'
 }))
 app.use(express.urlencoded({ extended: true }))
-
-const postLogin = require('./routes/postLogin')
-
 app.use('/api/v2', postLogin)
-
 app.listen(port, () => { console.log('Server running!') })
