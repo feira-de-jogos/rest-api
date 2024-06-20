022f69ad7d01f72d1382ad1e331835ec91859588
Ederson Torresini <etorresini@ifsc.edu.br>
2024-06-20 14:42:24 -0300

    Adicionar namespaces no servidor

--- servidor.js
+++ servidor.js
@@ -1,8 +1,14 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const app = express()
+const { createServer } = require("http")
+const { Server } = require("socket.io")
 const port = process.env.PORT || 3000
+
+const app = express()
+const httpServer = createServer(app)
+const io = new Server(httpServer, { path: "/api/v2/machine/" })
+
 const postLogin = require('./routes/postLogin')
 
 app.use(cors({
@@ -14,5 +20,21 @@ app.use(cors({
   methods: 'POST'
 }))
 app.use(express.urlencoded({ extended: true }))
+
 app.use('/api/v2', postLogin)
-app.listen(port, () => { console.log('Server running!') })
+
+io.of('/vending-machine').on('connection', (socket) => {
+    socket.on('disconnect', () => { })
+})
+
+io.of('/arcade').on('connection', (socket) => {
+  console.log("Socket: " + socket.id)
+  
+  socket.on('state', (state) => {
+    console.log("Socket " + socket.id + ": " + state)
+  })
+
+  socket.on('disconnect', () => { })
+})
+
+httpServer.listen(port, () => { console.log('Server running!') })
