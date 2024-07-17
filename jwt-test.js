af95f49ab4a85f5f23c7d5e7856e456db33293b6
Léo Jung Neves <leo.jn13@aluno.ifsc.edu.br>
2024-07-16 21:24:26 -0300

    Testar autenticação com JWT HS256 com WebSocket e namespace

---
+++ jwt-test.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+const io = require('socket.io-client')
+const secretKey = process.env.TOKEN_SECRET_KEY_ARCADE
+
+const payload = { machine: 'arcade', id: 0 };
+const token = jwt.sign(payload, secretKey, { expiresIn: '365d' });
+const socket = io('https://feira-de-jogos.dev.br/arcade', { path: '/api/v2/machine', auth: { token } })
+
+socket.on('connect', () => {
+  console.log('Connected')
+  socket.emit('state', 'ready')
+  socket.disconnect()
+})
--- servidor.js
+++ servidor.js
@@ -3,11 +3,13 @@ const express = require('express')
 const cors = require('cors')
 const { createServer } = require('http')
 const { Server } = require('socket.io')
-const port = process.env.PORT || 3000
-
 const app = express()
 const httpServer = createServer(app)
 const io = new Server(httpServer, { path: '/api/v2/machine/' })
+const port = process.env.PORT || 3000
+const jwt = require('jsonwebtoken')
+const secretKeyVendingMachine = process.env.TOKEN_SECRET_KEY_VENDING_MACHINE
+const secretKeyArcade = process.env.TOKEN_SECRET_KEY_ARCADE
 
 const postLogin = require('./routes/postLogin')
 const postTransfer = require('./routes/postTransfer')
@@ -29,13 +31,37 @@ app.use('/api/v2', getGames)
 app.use('/api/v2', getStatement)
 app.use('/api/v2', getProducts)
 
+io.of('/vending-machine').use(async (socket, next) => {
+  try {
+    const token = socket.handshake.auth.token
+    jwt.verify(token, secretKeyVendingMachine)
+    next()
+  } catch (error) {
+    console.error('Authentication error', error)
+    next(new Error('Authentication error'))
+  }
+})
+
 io.of('/vending-machine').on('connection', (socket) => {
+  socket.on('state', (state) => {
+    console.log('Socket ' + socket.id + ': ' + state)
+  })
+
   socket.on('disconnect', () => { })
 })
 
-io.of('/arcade').on('connection', (socket) => {
-  console.log('Socket: ' + socket.id)
+io.of('/arcade').use(async (socket, next) => {
+  try {
+    const token = socket.handshake.auth.token
+    jwt.verify(token, secretKeyArcade)
+    next()
+  } catch (error) {
+    console.error('Authentication error', error)
+    next(new Error('Authentication error'))
+  }
+})
 
+io.of('/arcade').on('connection', (socket) => {
   socket.on('state', (state) => {
     console.log('Socket ' + socket.id + ': ' + state)
   })
