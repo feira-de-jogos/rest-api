927e97be72c2d36c54494f0c113e4754a27e8ef3
Ederson Torresini <etorresini@ifsc.edu.br>
2024-08-09 13:51:27 -0300

    Criar timer para cancelar MFA #8

--- routes/postDebit.js
+++ routes/postDebit.js
@@ -40,25 +40,17 @@ router.post('/debit', async (req, res) => {
     const userId = auth.rows[0].id
     const { machine, product } = req.body
 
-    // Código a seguir não alinhado com o fluxo definido:
-    // https://github.com/feira-de-jogos/docs/tree/main/v2#operação-de-débito
-
-    // Verifica se a maquina existe
     const machineSearch = await db.query('SELECT * from "machines" WHERE "id" = $1', [machine])
     if (machineSearch.rowCount === 0) {
-      // return res.sendStatus(403).message("maquina inexistente")
       return res.status(403).send("Maquina Inexistente");
     }
 
-    // Verifica se o produto existe e se ele pode ser comprado por arcade/vending-machine
     const productSearch = await db.query('SELECT "type" FROM "products" WHERE "id" = $1 AND (type = (SELECT "id" FROM "types" WHERE "name" = \'foods\' LIMIT 1) OR type = (SELECT "id" FROM "types" WHERE "name" = \'arcade\' LIMIT 1));', [product])
     if (productSearch.rowCount === 0) {
-      //return res.sendStatus(403).message("produto inexistente")
       return res.status(403).send("Produto Inexistente");
     }
     const machineType = productSearch.rows[0].type
 
-    // Verifica se o usu�rio tem dinheiro para comprar o produto
     const valueProductSearch = await db.query('SELECT "price" FROM "products" WHERE "id" = $1;', [product])
     productValue = valueProductSearch.rows[0].price;
 
@@ -69,39 +61,28 @@ router.post('/debit', async (req, res) => {
     revenues = parseInt(revenues.rows[0].sum)
 
     let BalanceValue = revenues - expenses;
-
     if (productValue > BalanceValue) {
-      // return res.sendStatus(402).message("saldo insuficiente")
       return res.status(402).send("Saldo Insuficiente");
     }
 
-    // Verifica se a m�quina esta ocupada
     const machineBusySearch = await db.query('SELECT "busy" FROM "machines" WHERE "id" = $1', [machine])
     if (machineBusySearch.rows[0].busy == true) {
       return res.status(403).send("Maquina Ocupada");
     }
 
     const typeSearch = await db.query('SELECT "name" FROM "types" WHERE "id" = $1', [machineType])
-    // Verifica se o produto � da vending machine 
     if (typeSearch.rows[0].name == 'foods') {
-      // Verifica se esta em estoque
       const stockSearch = await db.query('SELECT "slot", "quantity" FROM "stock" WHERE "machine" = $1 AND "product" = $2;', [machine, product])
       if (stockSearch.rowCount === 0 || stockSearch.rows[0].quantity == 0) {
         return res.status(403).send("Produto Fora de Estoque")
       }
-      
-      // Maquina fica ocupada
-      const updateMachineStatus = await db.query('UPDATE "machines" SET "busy" = true WHERE "id" = $1 ', [machine])
-      
-      // Gera o Mfa
-      mfa = Math.floor(Math.random() * (99 - 11 + 1)) + 11
 
-      //Insere a operação de débito
+      await db.query('UPDATE "machines" SET "busy" = true WHERE "id" = $1 ', [machine])
+
+      mfa = Math.floor(Math.random() * (99 - 11 + 1)) + 11
       const insertResult = await db.query('INSERT INTO "operations"("from", "to", "product", "value", "date", "mfa", "completed") VALUES($1, 1, $2, $3, NOW(), $4, false) RETURNING "id"', [userId, product, productValue, mfa])
       var operationId = insertResult.rows[0].id
-      
-      // Manda o MFA para unity
-      // Nao entendi esse codigo -- io.to('machine').emit('purchase', jsonString)
+
       let userName = await db.query('SELECT "name" FROM "people" WHERE "email" = $1 ', [email])
       userName = userName.rows[0].name
       let stateMfaObject = {
@@ -109,15 +90,13 @@ router.post('/debit', async (req, res) => {
         code: mfa,
         operation: operationId
       }
-      // let stateMfaJsonString = JSON.stringify(stateMfaObject);
+
       console.log("Enviando websocket:", [stateMfaObject])
-      //io.of('/vending-machine').emit('stateMFA', stateMfaJsonString)
-      //
-      // O motor escolhido é a columa 'slot' da tabela 'stock'
       io.of('/vending-machine').emit('stateMFA', stateMfaObject)
-      // io.of('/vending-machine').emit('stateReleasing', { product: slot, operation: 77 })
+      setTimeout(() => {
+        db.query('UPDATE "machines" SET "busy" = false WHERE "id" = $1', [machine]), 30000
+      })
     } else if (typeSearch.rows[0].name == 'arcade') {
-      // Insere a opera��o de d�bito
       const insertResult = await db.query('INSERT INTO "operations"("from", "to", "product", "value", "date", "completed") VALUES($1, 1, $2, $3, NOW(), false) RETURNING "id"', [userId, product, productValue])
       var operationId = insertResult.rows[0].id
 
